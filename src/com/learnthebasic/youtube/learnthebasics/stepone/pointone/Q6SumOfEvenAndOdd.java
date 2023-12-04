90fa26d8d30ad5cb57dca5c0b90a90a2cd76e3c9
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-12-04 23:50:03 +0530

    Q6

---
+++ src/com/learnthebasic/youtube/learnthebasics/stepone/pointone/Q6SumOfEvenAndOdd.java
@@ -0,0 +1,25 @@
+package com.learnthebasic.youtube.learnthebasics.stepone.pointone;
+
+import java.util.Scanner;
+
+public class Q6SumOfEvenAndOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int even = 0;
+        int odd = 0;
+        int digit = 0;
+
+        while (n>0) {
+            digit = n%10;
+            n = n/10;
+            if (digit%2!=0) {
+                odd = digit + odd;
+            } else {
+                even = digit + even;
+            }
+        }
+        System.out.println(even+" "+odd);
+    }
+}
