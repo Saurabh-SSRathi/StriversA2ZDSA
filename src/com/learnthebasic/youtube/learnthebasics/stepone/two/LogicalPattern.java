c434eb7a1603182b21564217d9589aa5c6c76a2f
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-12-06 23:59:21 +0530

    patternOne

--- src/com/learnthebasic/youtube/learnthebasics/stepone/two/LogicalPattern.java
+++ src/com/learnthebasic/youtube/learnthebasics/stepone/two/LogicalPattern.java
@@ -3,15 +3,30 @@ package com.learnthebasic.youtube.learnthebasics.stepone.two;
 public class LogicalPattern {
 
     public static void patternOne(int n) {
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
+        for (int row=0; row<n; row++) {
+            for (int col=0; col<n; col++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 
+    public static void patternTwo(int n) {
+        for(int row=1; row<=n; row++) {
+            for(int col=1; col<=row; col++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void patternThree(int n) {
+
+    }
+
+
     public static void main(String[] args) {
-        LogicalPattern.patternOne(3);
+        //patternOne(3);
+        patternTwo(3);
     }
 }
