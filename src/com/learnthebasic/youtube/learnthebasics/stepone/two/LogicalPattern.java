95a30819ed635c80fd01e7e67b0fcbd769032246
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-12-10 00:05:04 +0530

    pattern12

--- src/com/learnthebasic/youtube/learnthebasics/stepone/two/LogicalPattern.java
+++ src/com/learnthebasic/youtube/learnthebasics/stepone/two/LogicalPattern.java
@@ -20,13 +20,30 @@ public class LogicalPattern {
         }
     }
 
-    public static void patternThree(int n) {
+    public static void patternTwelve(int n) {
+        // Write your code here.
+        for(int row=1; row<=n; row++) {
 
+            //no
+            for(int col=1; col<=row; col++) {
+                System.out.print(col+" ");
+            }
+            //space
+            for(int col=1 ; col<=(2*n-2*row); col++){
+                System.out.print(" ");
+            }
+            //no
+            for(int col=row; col>=1; col--) {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
     }
 
 
     public static void main(String[] args) {
         //patternOne(3);
-        patternTwo(3);
+        //patternTwo(3);
+        patternTwelve(3);
     }
 }
